5dc6e0c9f116918f2f6b8c034d610c4b53cfb80c
Samavedam Phani Kumar <phaniprince849@gmail.com>
2026-09-24 10:23:09 +0530

    By Using Arrays

---
+++ JavaFIles/src/com/arrays/TestArrayDemo2.java
@@ -0,0 +1,15 @@
+//Date:-24-09-2026
+//Class Program
+//Write a Program to print the reverse of an arrya
+package com.arrays;
+
+public class TestArrayDemo2 {
+
+	public static void main(String[] args) {
+		int[] arr= {1,2,3,4,5};
+		for(int i=arr.length-1;i>=0;i--) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+
+}
